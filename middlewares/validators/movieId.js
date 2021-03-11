8e626afaef23395c79e788959496b608c16014dc
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-11 22:58:18 +0300

    Fix: celebrate check-up params

--- middlewares/validators/movieId.js
+++ middlewares/validators/movieId.js
@@ -2,6 +2,6 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.movieIdValidator = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().length(24).hex(),
+    movieId: Joi.string().required().length(24).hex(),
   }),
 });
